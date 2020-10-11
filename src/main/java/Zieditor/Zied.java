7fbdc9aa8a525470f85faab9d672213abf8c809c
zivodvok2 <zivodvok@gmail.com>
2020-10-11 19:08:54 +0000

    initial files

---
+++ src/main/java/Zieditor/Zied.java
@@ -0,0 +1,14 @@
+package Zieditor;
+import javax.swing.*;
+
+public class Zied extends JFrame{
+	public Zied(){
+		setSize(300, 100);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        }
+	public static void main(String[] arguments)
+	{
+		Zied sf=new Zied();
+	}
+}
